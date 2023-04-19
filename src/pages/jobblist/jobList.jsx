90617386ce2ce39fc824e69ae4009757c5c59068
mzffrtp <mzffr.tpgl@gmail.com>
2023-04-20 00:10:25 +0200

    job listing done

--- src/pages/jobblist/jobList.jsx
+++ src/pages/jobblist/jobList.jsx
@@ -2,12 +2,12 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import useApi from "../../components/hooks/useApi";
 import { setJobs } from "../../redux/jobSlice";
-import { ToastContainer, toast } from "react-toastify"
+import { toast } from "react-toastify"
 
 export default function JobbList() {
     const api = useApi();
     const dispatch = useDispatch();
-    const {jobState} = useSelector(state => state);
+    const { jobState } = useSelector(state => state);
     console.log(jobState);
 
     useEffect(() => {
@@ -28,66 +28,66 @@ export default function JobbList() {
                     progress: undefined,
                     theme: "colored",
                 });
-
             })
     }, [])
     return (
-       <>
-       <h3>{jobState.jobs.length} jobs found</h3>
-       <section className="listSection">
-            {
-                !jobState.initialized ? (
-                    <>
-                    <p>Loading ...</p>
-                    {
-                        toast.info('Loading your jobs!', {
-                            position: "top-right",
-                            autoClose: 1000,
-                            hideProgressBar: false,
-                            closeOnClick: true,
-                            pauseOnHover: true,
-                            draggable: true,
-                            progress: undefined,
-                            theme: "dark",
-                            })
-                    }</>
+        <>
+            <h3 className="jobCount">{jobState.jobs.length} jobs found</h3>
+            <section className="listSection">
+                {
+                    !jobState.initialized ? (
+                        <>
+                            <p>Loading ...</p>
+                            {
+                                toast.info('Loading your jobs!', {
+                                    position: "top-right",
+                                    autoClose: 1000,
+                                    hideProgressBar: false,
+                                    closeOnClick: true,
+                                    pauseOnHover: true,
+                                    draggable: true,
+                                    progress: undefined,
+                                    theme: "dark",
+                                })
+                            }
+                        </>
 
-                ) : (
-                   jobState.jobs.map((job,id)=>
-                   <div key={id}>
-                    <div className="jobCard">
-                        <div className="head">
-                            <div className="letter">
-                                <p>{job.company[0]}</p>
-                            </div>
-                            <div>
-                                <p>{job.position}</p>
-                                <p>{job.company}</p>
-                            </div>
-                        </div>
-                        <div className="body">
-                            <div className="field">
-                                <img alt=""/>
-                                <p>{job.location}</p>
-                            </div>
-                            <div className="field">
-                                <img alt=""/>
-                                <p>{job.dateApplied}</p>
-                            </div>
-                            <div className="field">
-                                <img alt=""/>
-                                <p>{job.type}</p>
+                    ) : (
+                        jobState.jobs.map((job, id) =>
+                            <div key={id}>
 
-                            </div>
-                            <div className="field">
-                                <img alt=""/>
-                            </div>
-                        </div>
-                    </div>
-                   </div>)
-                )
-            }
-       </section>
-       </>
+                                <div className="flip-card">
+                                    <div className="flip-card-inner">
+                                        <div className="flip-card-front">
+                                            <div className="title">
+                                                    <p>{job.position}</p>
+                                                    <p>{job.company}</p>
+                                            </div>
+                                            <div className="status">
+                                                <p className={job.status}>{job.status}</p>
+                                            </div>
+                                        </div>
+                                        <div className="flip-card-back">
+                                            <div className="field">
+                                            <i className="fa-solid fa-location-dot"></i>
+                                                <p>{job.location}</p>
+                                            </div>
+                                            <div className="field">
+                                            <i className="fa-regular fa-calendar"></i>
+                                                <p>{job.dateApplied}</p>
+                                            </div>
+                                            <div className="field">
+                                            <i className="fa-solid fa-briefcase"></i>
+                                                <p>{job.type}</p>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>)
+                    )
+                }
+            </section>
+        </>
     )
 }
